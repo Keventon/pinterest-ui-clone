a1142809c75aff1f67da72487a26c36f6fe3d1d7
Keventon <kevertonguimaraes19@gmail.com>
2024-02-28 20:14:03 -0300

    criado skeleton na splash

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import { theme } from "@/theme";
-import { transform } from "@babel/core";
 import { useEffect } from "react";
 import { StyleSheet, View, useWindowDimensions } from "react-native";
+
+import { Skeleton } from "moti/skeleton";
+
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -14,6 +16,12 @@ export default function Splash() {
   const logoPostionY = useSharedValue(0);
 
   const dimensions = useWindowDimensions();
+
+  const skeletonColors = [
+    theme.colors.gray[600],
+    theme.colors.gray[700],
+    theme.colors.gray[600],
+  ];
 
   const logoAnimatedStyles = useAnimatedStyle(() => ({
     transform: [{ scale: logoScale.value }, { translateY: logoPostionY.value }],
@@ -34,6 +42,34 @@ export default function Splash() {
     );
   }
 
+  function filters() {
+    return Array.from({ length: 10 }).map((_, index) => (
+      <Skeleton
+        key={index}
+        width={60}
+        height={26}
+        radius={6}
+        colors={skeletonColors}
+      />
+    ));
+  }
+
+  function boxes(column: "left" | "right") {
+    const rest = column === "left" ? 0 : 1;
+
+    return Array.from({ length: 20 })
+      .filter((_, index) => index % 2 === rest)
+      .map((_, index) => {
+        const height = index % 2 === (column === "left" ? 0 : 1) ? 200 : 300;
+
+        return (
+          <Animated.View key={index} style={[styles.box, { height }]}>
+            <Skeleton colors={skeletonColors} width="100%" height={height} />
+          </Animated.View>
+        );
+      });
+  }
+
   useEffect(() => {
     logoAnimation();
   }, []);
@@ -44,6 +80,13 @@ export default function Splash() {
         style={[styles.logo, logoAnimatedStyles]}
         source={require("@/assets/logo.png")}
       />
+
+      <View style={styles.header}>{filters()}</View>
+
+      <View style={styles.boxes}>
+        <View style={styles.column}>{boxes("left")}</View>
+        <View style={styles.column}>{boxes("right")}</View>
+      </View>
     </View>
   );
 }
@@ -54,9 +97,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: theme.colors.black,
+    paddingHorizontal: 12,
   },
   logo: {
     width: 64,
     height: 64,
+  },
+  boxes: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+  },
+  box: {
+    width: "100%",
+    backgroundColor: theme.colors.gray[600],
+    borderRadius: 16,
+  },
+  column: {
+    flex: 1,
+    gap: 12,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 16,
+    paddingBottom: 12,
   },
 });
